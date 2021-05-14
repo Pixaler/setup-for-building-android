a0edc9c775486cbc8eb5387f882003d7ff02e0a2
Pixaler <gnidko02@gmail.com>
2021-05-14 17:59:46 +0300

    Add fix problem with jack.

---
+++ setup(v0.2).sh
@@ -0,0 +1,37 @@
+#I create this script with elementaryOS 5.1.7(Ubuntu 18.04)
+#But you can try on another distro
+#update system
+apt update
+apt upgrade
+
+#package and java
+apt install ccache git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
+imagemagick python3-virtualenv openjdk-8-jdk 
+ 
+#installing repo and rbuild
+mkdir ~/bin
+export PATH=~/bin:$PATH #add to .bashrc or .zschrc
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+curl https://raw.githubusercontent.com/meizucustoms/rbuild/main/rbuild > ~/bin/rbuild #i prefer rbuild script for building android
+chmod +x ~/bin/rbuild
+chmod a+x ~/bin/repo
+
+virtualenv --python=/usr/bin/python2.7 ~/venv #create virual enviroment Python 2.7
+
+#working dir
+mkdir lineage15.1
+cd lineage15.1
+#auth in git
+git config --global user.name Vladimir Gnidko
+git config --global user.email gnidko02@gmail.com
+
+#working with repo
+repo init 
+mkdir -p .repo/local_manifests
+curl https://raw.githubusercontent.com/meizucustoms/local_manifest/master/mzlocal_oreo.xml > .repo/local_manifests/mz.xml
+repo sync -c -j $(nproc) --no-tags
+
+#When Jack diagnose gives error 35 (SSL error) the following steps worked for me
+
+
+
