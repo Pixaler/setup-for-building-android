5f0058bdf5fdd75aed322597ffff19495394f406
Pixaler <gnidko02@gmail.com>
2021-05-17 09:38:55 +0300

    Update setup-v0.4.sh

--- setup-v0.4.sh
+++ setup-v0.4.sh
@@ -5,8 +5,7 @@ sudo apt update
 sudo apt upgrade
 
 #package and java
-sudo apt install ccache git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
-imagemagick virtualenv openjdk-8-jdk python2 libncurses5
+sudo apt install ccache git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig imagemagick virtualenv openjdk-8-jdk python2 libncurses5
  
 #installing repo and rbuild
 mkdir ~/bin
